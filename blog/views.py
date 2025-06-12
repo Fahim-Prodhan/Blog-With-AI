13fed0264404ecf0b649db13d17750a7b949c209
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2025-06-13 05:05:53 +0600

    Crud done with rest framework

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from blog.models import *
+from blog.serializers import *
 
 # Create your views here.
+class BlogViewSets(viewsets.ModelViewSet):
+    queryset=Blog.objects.all()
+    serializer_class=BlogSerializer
+    
